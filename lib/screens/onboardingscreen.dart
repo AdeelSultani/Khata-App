7bd758d7c2601277272505945b6c854363572a18
Win 10 <adeelsultani68@gmail.com>
2025-11-29 21:03:46 +0500

    Initial commit

---
+++ lib/screens/onboardingscreen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _controller,
+              children: [
+                buildPage(
+                  image: "assets/images/slide1.png",
+                  title: "Gain total control of your money",
+                  subtitle: "Become your own money manager and make every cent count",
+                ),
+                buildPage(
+                  image: "assets/images/slide2.png",
+                  title: "Know where your money goes",
+                  subtitle: "Track your transaction easily, with categories and financial report",
+                ),
+                buildPage(
+                  image: "assets/images/slide3.png",
+                  title: "Planning ahead",
+                  subtitle: "Setup your budget for each category so you in control",
+                ),
+              ],
+            ),
+          ),
+
+          // PAGE INDICATOR (the dots)
+          SmoothPageIndicator(
+            controller: _controller,
+            count: 3,
+            effect: ExpandingDotsEffect(
+              activeDotColor: Colors.purple,
+              dotColor: Colors.grey.shade300,
+              dotHeight: 5,
+              dotWidth: 5,
+            ),
+          ),
+          SizedBox(height: 25),
+
+          // SIGN UP BUTTON
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:Color(0XFF7F3DFF),
+                minimumSize: Size(double.infinity, 55),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onPressed: () {},
+              child: Text("Sign Up",style: TextStyle(color: Color(0xffFCFCFC)),),
+            ),
+          ),
+
+          // LOGIN BUTTON
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Color(0XFFEEE5FF), width: 2),
+                minimumSize: Size(double.infinity, 55),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onPressed: () {},
+              child: Text("Login", style: TextStyle(color: Color(0xff7F3DFF))),
+            ),
+          ),
+
+          SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPage({required String image, required String title, required String subtitle}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(image, height: 250),
+
+          SizedBox(height: 20),
+
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+
+          SizedBox(height: 10),
+
+          Text(
+            subtitle,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 14, color: Colors.grey),
+          ),
+        ],
+      ),
+    );
+  }
+}
